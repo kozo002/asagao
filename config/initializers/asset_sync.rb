72b0b1505030cc1dbc320b95405042c8b8c22cf6
kozo yamagata <tune002@gmail.com>
2014-07-01 22:03:53 +0900

    AssetSyncがインストールされていない環境でエラーしないようにした

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,11 +1,13 @@
-AssetSync.configure do |config|
-  config.fog_provider = 'AWS'
-  config.fog_directory = Rails.application.secrets.aws_s3_bucket
-  config.aws_access_key_id = Rails.application.secrets.aws_access_key_id
-  config.aws_secret_access_key = Rails.application.secrets.aws_secret_access_key
-  config.existing_remote_files = 'delete'
-  config.fog_region = 'ap-northeast-1'
-  config.gzip_compression = true
-  config.manifest = true
-  config.fail_silently = true
+if defined?(AssetSync)
+  AssetSync.configure do |config|
+    config.fog_provider = 'AWS'
+    config.fog_directory = Rails.application.secrets.aws_s3_bucket
+    config.aws_access_key_id = Rails.application.secrets.aws_access_key_id
+    config.aws_secret_access_key = Rails.application.secrets.aws_secret_access_key
+    config.existing_remote_files = 'delete'
+    config.fog_region = 'ap-northeast-1'
+    config.gzip_compression = true
+    config.manifest = true
+    config.fail_silently = true
+  end
 end
