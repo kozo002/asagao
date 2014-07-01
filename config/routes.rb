71b8c0bba9d43ac70731124e65ac0e6fa7380ec3
kozo yamagata <tune002@gmail.com>
2014-07-01 23:00:29 +0900

    ログインページをbootstrapで作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
 
   namespace :admin do
     root to: 'pages#home'
