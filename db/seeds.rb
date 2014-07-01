d932e0b1a2866d464900e155d1085e11f98b1f6e
kozo yamagata <tune002@gmail.com>
2014-07-01 22:23:27 +0900

    Adminを作成するseedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Admin.count == 0
+  admin = Admin.new(
+    email: 'sample@example.com',
+    password: 'samplepass',
+    password_confirmation: 'samplepass'
+  )
+  admin.skip_confirmation!
+  admin.save!
+end
